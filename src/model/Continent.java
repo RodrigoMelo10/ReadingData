a34d661fe568c8a66c0b2e50eff2e8f7b1bb5f57
RodrigoMelo10 <digoliveira10@yahoo.com.br>
2020-04-05 18:07:33 +0100

    Continent ENUM done

--- src/model/Continent.java
+++ src/model/Continent.java
@@ -1,5 +1,24 @@
 package model;
 
-public enum Continent {
 
+//Creating 	ENUM for continent class
+public enum Continent {
+	
+	Europe("Europe"), Asia("Asia"), Antarctica("Antarctica"), South_America("South America"), North_America("North America"), Oceania("Oceania"), Africa("Africa");
+	
+	// DO NOT CHANGE DE VALUE
+	private final String continent;
+
+	private Continent(String continent) {
+		this.continent = continent;
+	}
+//RETURNING VALUE
+	@Override
+	public String toString() {
+		return this.continent;
+	}
+	
+	
 }
+
+
