f430aff74fd34457b67742069493a296beffc622
RodrigoMelo10 <digoliveira10@yahoo.com.br>
2020-04-05 17:15:55 +0100

    finishing interface country DAO

--- src/dao/CountryDAO.java
+++ src/dao/CountryDAO.java
@@ -1,5 +1,14 @@
 package dao;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Country;
 
 public interface CountryDAO {
 
+	public ArrayList<Country> getCountries();
+	public boolean saveCountry(Country country);
+	public List<Country> searchFromCountryName(String name);
+	public List<Country> searchFromCountryCode(String code);
+	
 }
